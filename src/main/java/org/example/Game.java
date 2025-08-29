d20148d2a547968bc75ecb7ec693be15bf5d4fda
Alena Deikun <alenadeikun02@gmail.com>
2025-08-29 15:16:04 +0300

    Add class with main logic and deleted 2 classes

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,10 +1,141 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
-    public void play() {
-        Word word = new Word();
-        word.select();
-        Letter letter = new Letter();
-        letter.input();
-    }
+     private int mistakesCount = 0;
+     private final int maxAttempts = 6;
+     private String selectedWord;
+     private String currentState;
+     private List <Character> usedLetters = new ArrayList<>();
+
+     public Game(String selectedWord) {
+         this.selectedWord = selectedWord;
+         this.currentState = "_".repeat(selectedWord.length());
+     }
+
+     public void printCurrentState() {
+         System.out.println("Загаданное слово: " + currentState);
+     }
+
+     public void addLetter(char letter) {
+         if (usedLetters.contains(letter)) {
+             System.out.println("\n\nВы уже пробовали эту букву!");
+         }
+
+         usedLetters.add(letter);
+
+         ArrayList<Integer> occurrenceIndices = new ArrayList<>();
+         for (int i = 0; i < selectedWord.length(); i++) {
+             if (letter == selectedWord.charAt(i)) {
+                 occurrenceIndices.add(i);
+             }
+         }
+
+         if (!occurrenceIndices.isEmpty()) {
+             StringBuilder stringBuilder = new StringBuilder(currentState);
+
+             for (int i : occurrenceIndices) {
+                     stringBuilder.setCharAt(i, letter);
+             }
+
+             currentState = stringBuilder.toString();
+             System.out.println("Отлично! Буква есть в слове.");
+             System.out.println("Использованные буквы: " + usedLetters);
+         } else {
+             mistakesCount++;
+             System.out.println("Такой буквы нет.");
+             System.out.println("Использованные буквы: " + usedLetters);
+             System.out.println("Ошибок: " + mistakesCount + " из " + maxAttempts);
+         }
+     }
+
+     public void hangman() {
+         if (mistakesCount == 0) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 1) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 2) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|   |");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 3) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|  /|");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 4) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|  /|\\");
+             System.out.println("|");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 5) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|  /|\\");
+             System.out.println("|  /");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+
+         if (mistakesCount == 6) {
+             System.out.println("+---+");
+             System.out.println("|   |");
+             System.out.println("|   O");
+             System.out.println("|  /|\\");
+             System.out.println("|  / \\");
+             System.out.println("|");
+             System.out.println("=========");
+         }
+     }
+
+     public boolean isWordGuessed() {
+         return !currentState.contains("_");
+     }
+
+     public int getMistakesCount() {
+         return mistakesCount;
+     }
+
+     public int getMaxAttempts() {
+         return maxAttempts;
+     }
+
+     public String getSelectedWord() {
+         return selectedWord;
+     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
         boolean running = true;
 
         while (running) {
-            System.out.println("\nДобро пожаловать в игру 'Виселица'!");
-            System.out.println("\n--- Меню ---");
+            System.out.println("\n\nДобро пожаловать в игру 'Виселица'!");
+            System.out.println("---- Меню ----");
             System.out.println("1. Начать игру");
             System.out.println("2. Выйти");
             System.out.print("Введите свой выбор: ");
@@ -20,24 +24,79 @@ public class Main {
             try {
                 choice = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
-                System.out.println("Неккоректное значение. Пожалуйта, введите цифру.");
+                System.out.println("\n\nНеккоректное значение. Пожалуйта, введите цифру.");
                 continue;
             }
 
             switch (choice) {
                 case 1:
-                    System.out.println("Вы выбрали начать игру.");
-                    game.play();
+                    startGame();
                     break;
                 case 2:
-                    System.out.println("Выход из игры. Досвидания!");
+                    System.out.println("\nВыход из игры. Досвидания!");
                     running = false;
                     break;
                 default:
-                    System.out.println("Неккоректный выбор. Пожалуйста, попробуйте еще раз.");
+                    System.out.println("\n\nНеккоректный выбор. Пожалуйста, попробуйте еще раз.");
             }
         }
-
         scanner.close();
+    }
+
+    public static void startGame() {
+        Game game = new Game(selectWord());
+        while (game.getMistakesCount() < game.getMaxAttempts() && !game.isWordGuessed()) {
+            game.printCurrentState();
+            game.hangman();
+            var letter = inputLetter();
+            game.addLetter(letter);
+        }
+
+        if (game.isWordGuessed()) {
+            System.out.println("\n\nПоздравляю! Вы отгадали слово: " + game.getSelectedWord());
+        } else {
+            System.out.println("\n\nИгра окончена. Загаданное слово было: " + game.getSelectedWord());
+        }
+    }
+
+    //word
+    public static String selectWord() {
+        try {
+            List<String> words = Files.readAllLines(Paths.get("src/main/resources/dictionary.txt"));
+            String selectedWord = words.get(ThreadLocalRandom.current().nextInt(words.size()));
+            System.out.print("\n\nСлово загадано! \nДлина слова: " + selectedWord.length() + "\n");
+            return selectedWord.trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    //letter
+    public static char inputLetter() {
+        System.out.print("Введите букву: ");
+        String input = scanner.nextLine();
+        validate(input);
+        return input.charAt(0);
+    }
+
+    private static void validate(String input) {
+        if (input == null || input.isEmpty()) {
+            System.out.println("\n\nОШИБКА: Ввод не может быть пустым!");
+            return;
+        }
+
+        if (input.length() == 1) {
+            char letter = input.charAt(0);
+
+            if (letter >= 'а' && letter <= 'я') {
+                System.out.println(" ");
+            } else {
+                System.out.println("\n\nОШИБКА: Нужно ввести строчную русскую букву!");
+            }
+
+        } else {
+            System.out.println("\n\nОШИБКА: Нужно ввести одну букву!");
+        }
     }
 }
